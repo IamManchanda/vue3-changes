370839a5aa2771fe7e6113a711b2ba4ee1267cc5
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-09-20 00:41:03 +0530

    Woah! Multi v-model bindings

---
+++ src/components/SalutationName.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <select
+      name="salutation"
+      @input="$emit('update:salutation', $event.target.value)"
+    >
+      <option value="">-</option>
+      <option
+        v-for="item of salutations"
+        :value="item"
+        :key="item"
+        :selected="salutation === item"
+      >
+        {{ item }}
+      </option>
+    </select>
+    <input
+      @input="$emit('update:name', $event.target.value)"
+      :value="name"
+      type="text"
+      name="name"
+    />
+  </div>
+</template>
+
+<script>
+const salutations = ["Ms.", "Mrs.", "Miss", "Mx.", "Mr.", "Dr."];
+
+export default {
+  props: {
+    salutation: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+  },
+  setup() {
+    return {
+      salutations,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+select {
+  margin-right: 0.5rem;
+}
+input {
+  width: 50%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,37 @@
 <template>
   <div>
+    <SalutationName
+      v-model:salutation="form.salutation"
+      v-model:name="form.name"
+    />
+    <pre>{{ form }}</pre>
+
+    <br />
+
     <base-input v-model:modelValue="myInput" />
-    {{ myInput }}
+    <pre>
+      {{ myInput }}
+    </pre>
   </div>
 </template>
 
 <script>
 import { reactive, toRefs } from "vue";
 import BaseInput from "@/components/ui/base-input";
+import SalutationName from "@/components/SalutationName";
 
 export default {
   components: {
     BaseInput,
+    SalutationName,
   },
   setup() {
     const state = reactive({
       myInput: "",
+      form: {
+        salutation: "",
+        name: "",
+      },
     });
     return { ...toRefs(state) };
   },
